9247c24f1eb6da0e8de58632d5436ec41c060e32
Eragon67360 <thomas-moser@orange.fr>
2024-06-03 17:51:31 +0200

    fix(route.ts): update SQL query to delete from 'urls' table instead of 'entries' table fix(vercel.json): change cron schedule from running every minute to running once a week on Sundays at midnight

--- app/api/delete-expired-entries/route.ts
+++ app/api/delete-expired-entries/route.ts
@@ -6,7 +6,7 @@ export async function POST() {
     const client = await pool.connect();
 
     try {
-        const result = await pool.query('DELETE FROM entries WHERE expires_at <= NOW()');
+        const result = await pool.query('DELETE FROM urls WHERE expires_at <= NOW()');
         return NextResponse.json({ message: 'Expired entries deleted', deletedCount: result.rowCount }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ message: 'Error deleting expired entries', error: error }, { status: 500 });
